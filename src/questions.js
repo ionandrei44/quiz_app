195ee858f606e41ef0f48696526cbcfd50a18d8a
Andrei <44devtests@gmail.com>
2022-08-23 21:24:48 +0300

    final project

---
+++ src/questions.js
@@ -0,0 +1,214 @@
+export const questions = [
+  {
+    questionText: "How long is an Olympic swimming pool (in meters)",
+    answers: [
+      {
+        answerText: "50",
+        correctAnswer: true,
+      },
+      {
+        answerText: "60",
+        correctAnswer: false,
+      },
+      {
+        answerText: "75",
+        correctAnswer: false,
+      },
+      {
+        answerText: "100",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText:
+      "Which country do cities of Perth, Adelade & Brisbane belong to?",
+    answers: [
+      {
+        answerText: "France",
+        correctAnswer: false,
+      },
+      {
+        answerText: "Portugal",
+        correctAnswer: false,
+      },
+      {
+        answerText: "Germany",
+        correctAnswer: false,
+      },
+      {
+        answerText: "Australia",
+        correctAnswer: true,
+      },
+    ],
+  },
+  {
+    questionText: "What geometric shape is generally used for stop signs?",
+    answers: [
+      {
+        answerText: "Square",
+        correctAnswer: false,
+      },
+      {
+        answerText: "Octagon",
+        correctAnswer: true,
+      },
+      {
+        answerText: "Rectangle",
+        correctAnswer: false,
+      },
+      {
+        answerText: "Triangle",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText: "What is cynophobia?",
+    answers: [
+      {
+        answerText: "Fear of spiders",
+        correctAnswer: false,
+      },
+      {
+        answerText: "Fear of darkness",
+        correctAnswer: false,
+      },
+      {
+        answerText: "Fear of dogs",
+        correctAnswer: true,
+      },
+      {
+        answerText: "Fear of heights",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText: "How many languages are written from right to left?",
+    answers: [
+      {
+        answerText: "12",
+        correctAnswer: true,
+      },
+      {
+        answerText: "10",
+        correctAnswer: false,
+      },
+      {
+        answerText: "15",
+        correctAnswer: false,
+      },
+      {
+        answerText: "1",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText: "Which animal can be seen on the Porsche logo?",
+    answers: [
+      {
+        answerText: "Fox",
+        correctAnswer: false,
+      },
+      {
+        answerText: "Wolf",
+        correctAnswer: false,
+      },
+      {
+        answerText: "Horse",
+        correctAnswer: true,
+      },
+      {
+        answerText: "Bull",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText: "What is the name of the largest ocean on earth?",
+    answers: [
+      {
+        answerText: "Pacific Ocean",
+        correctAnswer: true,
+      },
+      {
+        answerText: "Atlantic Ocean",
+        correctAnswer: false,
+      },
+      {
+        answerText: "Indian Ocean",
+        correctAnswer: false,
+      },
+      {
+        answerText: "Artic Ocean",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText:
+      "Demolition of the Berlin wall separating East and West Germany began in what year?",
+    answers: [
+      {
+        answerText: "1980",
+        correctAnswer: false,
+      },
+      {
+        answerText: "1989",
+        correctAnswer: true,
+      },
+      {
+        answerText: "1992",
+        correctAnswer: false,
+      },
+      {
+        answerText: "1990",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText: "What is the rarest M&M color?",
+    answers: [
+      {
+        answerText: "Brown",
+        correctAnswer: true,
+      },
+      {
+        answerText: "Green",
+        correctAnswer: false,
+      },
+      {
+        answerText: "Red",
+        correctAnswer: false,
+      },
+      {
+        answerText: "Blue",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText: "Which country invented ice cream?",
+    answers: [
+      {
+        answerText: "India",
+        correctAnswer: false,
+      },
+      {
+        answerText: "China",
+        correctAnswer: true,
+      },
+      {
+        answerText: "England",
+        correctAnswer: false,
+      },
+      {
+        answerText: "Brazil",
+        correctAnswer: false,
+      },
+    ],
+  },
+];
